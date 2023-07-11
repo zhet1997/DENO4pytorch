f1cbb806651410c7d15099b2ecc1f563f86bb040
8905 <8905>
2023-07-11 14:05:49 +0800

    nothing

--- Demo/Rotor37_2d/run_Trans.py
+++ Demo/Rotor37_2d/run_Trans.py
@@ -25,8 +25,8 @@ import time
 
 import sys
 import yaml
-from utilizes_rotor37 import get_origin_old
-from run_MLP import get_grid, get_origin
+from utilizes_rotor37 import get_grid, get_origin, Rotor37WeightLoss
+from train_model.model_whole_life import WorkPrj, DLModelWhole, change_yml, add_yml
 from post_process.post_data import Post_2d
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
@@ -131,159 +131,160 @@ if __name__ == "__main__":
     ################################################################
     # configs
     ################################################################
-    for mode in [8, 10, 12, 14, 16]:
 
-        # name = 'Transformer_' + str(mode)
-        name = 'FNO_' + str(mode)
-        # name = 'Transformer'
-        work_path = os.path.join('work', name)
-        train_path = os.path.join(work_path, 'train')
-        isCreated = os.path.exists(work_path)
-        if not isCreated:
-            os.makedirs(work_path)
-            os.makedirs(train_path)
 
-        # 将控制台的结果输出到log文件
-        Logger = TextLogger(os.path.join(train_path, 'train.log'))
+    name = 'Transformer'
+    work_path = os.path.join('work', name)
+    train_path = os.path.join(work_path, 'train')
+    isCreated = os.path.exists(work_path)
+    if not isCreated:
+        os.makedirs(work_path)
+        os.makedirs(train_path)
 
-        if torch.cuda.is_available():
-            Device = torch.device('cuda:0')
-        else:
-            Device = torch.device('cpu')
+    # 将控制台的结果输出到log文件
+    Logger = TextLogger(os.path.join(train_path, 'train.log'))
 
-        design, fields = get_origin_old()
-        fields = fields[:, 0].transpose(0, 2, 3, 1)
+    if torch.cuda.is_available():
+        Device = torch.device('cuda:0')
+    else:
+        Device = torch.device('cpu')
 
-        in_dim = 28
-        out_dim = 5
-        ntrain = 700
-        nvalid = 200
+    # design, fields = get_origin_old()
+    # fields = fields[:, 0].transpose(0, 2, 3, 1)
 
-        batch_size = 32
-        epochs = 501
-        learning_rate = 0.001
-        scheduler_step = 400
-        scheduler_gamma = 0.1
+    design, fields = get_origin()
 
-        print(epochs, learning_rate, scheduler_step, scheduler_gamma)
+    in_dim = 28
+    out_dim = 5
+    ntrain = 2500
+    nvalid = 400
 
-        #这部分应该是重采样
-        #不进行稀疏采样
-        r_train = 1
-        h_train = int(((64 - 1) / r_train) + 1)
-        s_train = h_train
+    batch_size = 32
+    epochs = 1001
+    learning_rate = 0.001
+    scheduler_step = 800
+    scheduler_gamma = 0.1
 
-        r_valid = 1
-        h_valid = int(((64 - 1) / r_valid) + 1)
-        s_valid = h_valid
+    print(epochs, learning_rate, scheduler_step, scheduler_gamma)
 
-        ################################################################
-        # load data
-        ################################################################
+    #这部分应该是重采样
+    #不进行稀疏采样
+    r_train = 1
+    h_train = int(((64 - 1) / r_train) + 1)
+    s_train = h_train
 
-        input = torch.tensor(design, dtype=torch.float)
-        output = torch.tensor(fields, dtype=torch.float)
+    r_valid = 1
+    h_valid = int(((64 - 1) / r_valid) + 1)
+    s_valid = h_valid
 
-        print(input.shape, output.shape)
+    ################################################################
+    # load data
+    ################################################################
+    work = WorkPrj(work_path)
 
-        train_x = input[:ntrain]
-        train_y = output[:ntrain, :, :]
-        valid_x = input[ntrain:ntrain + nvalid]
-        valid_y = output[ntrain:ntrain + nvalid, :, :]
+    input = torch.tensor(design, dtype=torch.float)
+    output = torch.tensor(fields, dtype=torch.float)
 
-        x_normalizer = DataNormer(train_x.numpy(), method='mean-std')
-        train_x = x_normalizer.norm(train_x)
-        valid_x = x_normalizer.norm(valid_x)
+    print(input.shape, output.shape)
 
-        y_normalizer = DataNormer(train_y.numpy(), method='mean-std')
-        train_y = y_normalizer.norm(train_y)
-        valid_y = y_normalizer.norm(valid_y)
+    train_x = input[:ntrain]
+    train_y = output[:ntrain, :, :]
+    valid_x = input[-nvalid: ,:]
+    valid_y = output[-nvalid: , :, :]
 
-        train_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(train_x, train_y),
-                                                   batch_size=batch_size, shuffle=True, drop_last=True)
-        valid_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(valid_x, valid_y),
-                                                   batch_size=batch_size, shuffle=False, drop_last=True)
+    x_normalizer = DataNormer(train_x.numpy(), method='mean-std')
+    train_x = x_normalizer.norm(train_x)
+    valid_x = x_normalizer.norm(valid_x)
 
-        ################################################################
-        #  Neural Networks
-        ################################################################
-        with open(os.path.join('transformer_config.yml')) as f:
-            config = yaml.full_load(f)
-            config = config['Rotor37_2d']
+    y_normalizer = DataNormer(train_y.numpy(), method='mean-std')
+    train_y = y_normalizer.norm(train_y)
+    valid_y = y_normalizer.norm(valid_y)
 
-        config['fourier_modes'] = mode
+    train_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(train_x, train_y),
+                                               batch_size=batch_size, shuffle=True, drop_last=True)
+    valid_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(valid_x, valid_y),
+                                               batch_size=batch_size, shuffle=False, drop_last=True)
+    x_normalizer.save(work.x_norm)
+    y_normalizer.save(work.y_norm)
+    ################################################################
+    #  Neural Networks
+    ################################################################
+    with open(os.path.join('transformer_config.yml')) as f:
+        config = yaml.full_load(f)
+        config = config['Rotor37_2d']
 
-        # 建立网络
-        Tra_model = FourierTransformer(**config).to(Device)
-        FNO_model = FNO2d(in_dim=2, out_dim=config['n_targets'], modes=(16, 16), width=64, depth=4,
-                          padding=9, activation='gelu').to(Device)
-        MLP_model = FcnSingle(planes=(in_dim, 64, 64, config['n_targets']), last_activation=True).to(Device)
-        Net_model = predictor(trunc=FNO_model, branch=MLP_model, field_dim=out_dim).to(Device)
+    # 建立网络
+    Tra_model = FourierTransformer(**config).to(Device)
+    FNO_model = FNO2d(in_dim=2, out_dim=config['n_targets'], modes=(16, 16), width=64, depth=4,
+                      padding=9, activation='gelu').to(Device)
+    MLP_model = FcnSingle(planes=(in_dim, 64, 64, config['n_targets']), last_activation=True).to(Device)
+    Net_model = predictor(trunc=FNO_model, branch=MLP_model, field_dim=out_dim).to(Device)
 
-        # model_statistics = summary(Net_model, input_size=(batch_size, train_x.shape[1]), device=Device)
-        # Logger.write(str(model_statistics))
+    # model_statistics = summary(Net_model, input_size=(batch_size, train_x.shape[1]), device=Device)
+    # Logger.write(str(model_statistics))
 
-        # 损失函数
-        Loss_func = nn.MSELoss()
-        # Loss_func = nn.SmoothL1Loss()
-        # 优化算法
-        Optimizer = torch.optim.Adam(Net_model.parameters(), lr=learning_rate, betas=(0.7, 0.9), weight_decay=1e-7)
-        # 下降策略
-        Scheduler = torch.optim.lr_scheduler.StepLR(Optimizer, step_size=scheduler_step, gamma=scheduler_gamma)
-        # 可视化
-        Visual = MatplotlibVision(train_path, input_name=('x', 'y'), field_name=('p', 't', 'rho', 'alf', 'v'))
+    # 损失函数
+    # Loss_func = nn.MSELoss()
+    Loss_func = Rotor37WeightLoss()
+    # Loss_func = nn.SmoothL1Loss()
+    # 优化算法
+    Optimizer = torch.optim.Adam(Net_model.parameters(), lr=learning_rate, betas=(0.7, 0.9), weight_decay=1e-7)
+    # 下降策略
+    Scheduler = torch.optim.lr_scheduler.StepLR(Optimizer, step_size=scheduler_step, gamma=scheduler_gamma)
+    # 可视化
+    Visual = MatplotlibVision(train_path, input_name=('x', 'y'), field_name=('p', 't', 'rho', 'alf', 'v'))
+
+    star_time = time.time()
+    log_loss = [[], []]
+
+    ################################################################
+    # train process
+    ################################################################
+    # grid = get_grid()
+    from Utilizes.geometrics import gen_uniform_grid
+    grid = gen_uniform_grid(train_y[:1]).to(Device)
+    for epoch in range(epochs):
+
+        Net_model.train()
+        log_loss[0].append(train(train_loader, Net_model, Device, Loss_func, Optimizer, Scheduler))
+
+        Net_model.eval()
+        log_loss[1].append(valid(valid_loader, Net_model, Device, Loss_func))
+        print('epoch: {:6d}, lr: {:.3e}, train_step_loss: {:.3e}, valid_step_loss: {:.3e}, cost: {:.2f}'.
+              format(epoch, learning_rate, log_loss[0][-1], log_loss[1][-1], time.time() - star_time))
 
         star_time = time.time()
-        log_loss = [[], []]
+
+        if epoch > 0 and epoch % 10 == 0:
+            fig, axs = plt.subplots(1, 1, figsize=(15, 8), num=1)
+            Visual.plot_loss(fig, axs, np.arange(len(log_loss[0])), np.array(log_loss)[0, :], 'train_step')
+            Visual.plot_loss(fig, axs, np.arange(len(log_loss[0])), np.array(log_loss)[1, :], 'valid_step')
+            fig.suptitle('training loss')
+            fig.savefig(os.path.join(train_path, 'log_loss.svg'))
+            plt.close(fig)
 
         ################################################################
-        # train process
+        # Visualization
         ################################################################
-        # grid = get_grid()
-        from geometrics import gen_uniform_grid
-        grid = gen_uniform_grid(train_y[:1]).to(Device)
-        for epoch in range(epochs):
 
-            Net_model.train()
-            log_loss[0].append(train(train_loader, Net_model, Device, Loss_func, Optimizer, Scheduler))
+        if epoch > 0 and epoch % 100 == 0:
+            # print('epoch: {:6d}, lr: {:.3e}, eqs_loss: {:.3e}, bcs_loss: {:.3e}, cost: {:.2f}'.
+            #       format(epoch, learning_rate, log_loss[-1][0], log_loss[-1][1], time.time()-star_time))
+            train_source, train_true, train_pred = inference(train_loader, Net_model, Device)
+            valid_source, valid_true, valid_pred = inference(valid_loader, Net_model, Device)
 
-            Net_model.eval()
-            log_loss[1].append(valid(valid_loader, Net_model, Device, Loss_func))
-            print('epoch: {:6d}, lr: {:.3e}, train_step_loss: {:.3e}, valid_step_loss: {:.3e}, cost: {:.2f}'.
-                  format(epoch, learning_rate, log_loss[0][-1], log_loss[1][-1], time.time() - star_time))
+            torch.save({'log_loss': log_loss, 'net_model': Net_model.state_dict(), 'optimizer': Optimizer.state_dict()},
+                       os.path.join(train_path, 'latest_model.pth'))
 
-            star_time = time.time()
-
-            if epoch > 0 and epoch % 10 == 0:
-                fig, axs = plt.subplots(1, 1, figsize=(15, 8), num=1)
-                Visual.plot_loss(fig, axs, np.arange(len(log_loss[0])), np.array(log_loss)[0, :], 'train_step')
-                Visual.plot_loss(fig, axs, np.arange(len(log_loss[0])), np.array(log_loss)[1, :], 'valid_step')
-                fig.suptitle('training loss')
-                fig.savefig(os.path.join(train_path, 'log_loss.svg'))
+            for fig_id in range(5):
+                fig, axs = plt.subplots(out_dim, 3, figsize=(18, 25), num=2)
+                Visual.plot_fields_ms(fig, axs, train_true[fig_id], train_pred[fig_id])
+                fig.savefig(os.path.join(train_path, 'train_solution_' + str(fig_id) + '.jpg'))
                 plt.close(fig)
 
-            ################################################################
-            # Visualization
-            ################################################################
-
-            if epoch > 0 and epoch % 100 == 0:
-                # print('epoch: {:6d}, lr: {:.3e}, eqs_loss: {:.3e}, bcs_loss: {:.3e}, cost: {:.2f}'.
-                #       format(epoch, learning_rate, log_loss[-1][0], log_loss[-1][1], time.time()-star_time))
-                train_source, train_true, train_pred = inference(train_loader, Net_model, Device)
-                valid_source, valid_true, valid_pred = inference(valid_loader, Net_model, Device)
-
-                torch.save({'log_loss': log_loss, 'net_model': Net_model.state_dict(), 'optimizer': Optimizer.state_dict()},
-                           os.path.join(train_path, 'latest_model.pth'))
-
-                for fig_id in range(5):
-                    fig, axs = plt.subplots(out_dim, 3, figsize=(18, 25), num=2)
-                    Visual.plot_fields_ms(fig, axs, train_true[fig_id], train_pred[fig_id])
-                    fig.savefig(os.path.join(train_path, 'train_solution_' + str(fig_id) + '.jpg'))
-                    plt.close(fig)
-
-                for fig_id in range(5):
-                    fig, axs = plt.subplots(out_dim, 3, figsize=(18, 25), num=3)
-                    Visual.plot_fields_ms(fig, axs, valid_true[fig_id], valid_pred[fig_id])
-                    fig.savefig(os.path.join(train_path, 'valid_solution_' + str(fig_id) + '.jpg'))
-                    plt.close(fig)
+            for fig_id in range(5):
+                fig, axs = plt.subplots(out_dim, 3, figsize=(18, 25), num=3)
+                Visual.plot_fields_ms(fig, axs, valid_true[fig_id], valid_pred[fig_id])
+                fig.savefig(os.path.join(train_path, 'valid_solution_' + str(fig_id) + '.jpg'))
+                plt.close(fig)
 
