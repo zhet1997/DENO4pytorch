0fe22304a8445c176292d9169967d4fb548df54e
wqn <490760422@qq.com>
2023-11-15 21:39:06 +0800

    add cfd post

--- Demo/GVRB_2d/run_TNO_struct.py
+++ Demo/GVRB_2d/run_TNO_struct.py
@@ -130,7 +130,7 @@ if __name__ == "__main__":
 
 
     name = 'TNO'
-    work_path = os.path.join('work', name+str(2))
+    work_path = os.path.join('work', name+'_'+str(3))
     train_path = os.path.join(work_path)
     isCreated = os.path.exists(work_path)
     if not isCreated:
@@ -150,137 +150,140 @@ if __name__ == "__main__":
 
     design, fields, grids = get_origin(type='struct')  # 获取原始数据取原始数据
 
-    in_dim = 100
-    out_dim = 8
-    ntrain = 5000
-    nvalid = 900
-
-    batch_size = 32
-    epochs = 1001
-    learning_rate = 0.001
-    scheduler_step = 800
-    scheduler_gamma = 0.1
-    r1 = 1
-    print(epochs, learning_rate, scheduler_step, scheduler_gamma)
-    #这部分应该是重采样
-    #不进行稀疏采样
-
-
-    ################################################################
-    # load data
-    ################################################################
-    work = WorkPrj(work_path)
-    input = design
-    input = torch.tensor(input, dtype=torch.float)
-
-    # output = fields[:, 0, :, :, :].transpose((0, 2, 3, 1))
-    output = fields
-    output = torch.tensor(output, dtype=torch.float)
-
-    print(input.shape, output.shape)
-
-    train_x = input[:ntrain]
-    train_y = output[:ntrain]
-    # train_g = grids[:ntrain, ::r1]
-    valid_x = input[-nvalid:]
-    valid_y = output[-nvalid:]
-    # valid_g = grids[-nvalid:, ::r1]
-
-    x_normalizer = DataNormer(train_x.numpy(), method='mean-std')
-    train_x = x_normalizer.norm(train_x)
-    valid_x = x_normalizer.norm(valid_x)
-
-    y_normalizer = DataNormer(train_y.numpy(), method='mean-std')
-    train_y = y_normalizer.norm(train_y)
-    valid_y = y_normalizer.norm(valid_y)
-
-    train_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(train_x, train_y),
-                                               batch_size=batch_size, shuffle=True, drop_last=True)
-    valid_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(valid_x, valid_y),
-                                               batch_size=batch_size, shuffle=False, drop_last=True)
-
-
-    ################################################################
-    #  Neural Networks
-    ################################################################
-    with open(os.path.join('transformer_config_gvrb.yml')) as f:
-        config = yaml.full_load(f)
-        config = config['GVRB_2d']
-
-    # 建立网络
-    Tra_model = FourierTransformer(**config).to(Device)
-    # Tra_model = SimpleTransformer(**config).to(Device)
-    # FNO_model = FNO2d(in_dim=2, out_dim=config['n_targets'], modes=(16, 16), width=64, depth=4,
-    #                   padding=9, activation='gelu').to(Device)
-    MLP_model = FcnSingle(planes=(in_dim, 64, 64, config['n_targets']), last_activation=True).to(Device)
-    Net_model = predictor(trunc=Tra_model, branch=MLP_model, field_dim=out_dim).to(Device)
-
-    # model_statistics = summary(Net_model, input_size=(batch_size, train_x.shape[1]), device=Device)
-    # Logger.write(str(model_statistics))
-
-    # 损失函数
-    Loss_func = nn.MSELoss()
-    # Loss_func = Rotor37WeightLoss()
-    # Loss_func = nn.SmoothL1Loss()
-    # 优化算法
-    Optimizer = torch.optim.Adam(Net_model.parameters(), lr=learning_rate, betas=(0.7, 0.9), weight_decay=1e-7)
-    # 下降策略
-    Scheduler = torch.optim.lr_scheduler.StepLR(Optimizer, step_size=scheduler_step, gamma=scheduler_gamma)
-    # 可视化
-    Visual = MatplotlibVision(work_path, input_name=('x', 'y'), field_name=('ps', 'ts', 'rho', 'vx', 'vy', 'vz', 'tt1', 'tt2'))
-
-
-    star_time = time.time()
-    log_loss = [[], []]
-
-    ################################################################
-    # train process
-    ################################################################
-    # grid = get_grid()
-    # grid_real = get_grid()
-    # grid = gen_uniform_grid(train_y[:1]).to(Device)
-    for epoch in range(epochs):
-
-        Net_model.train()
-        log_loss[0].append(train(train_loader, Net_model, Device, Loss_func, Optimizer, Scheduler))
-
-        Net_model.eval()
-        log_loss[1].append(valid(valid_loader, Net_model, Device, Loss_func))
-        print('epoch: {:6d}, lr: {:.3e}, train_step_loss: {:.3e}, valid_step_loss: {:.3e}, cost: {:.2f}'.
-              format(epoch, learning_rate, log_loss[0][-1], log_loss[1][-1], time.time() - star_time))
-
-        star_time = time.time()
-
-        if epoch > 0 and epoch % 10 == 0:
-            fig, axs = plt.subplots(1, 1, figsize=(15, 8), num=1)
-            Visual.plot_loss(fig, axs, np.arange(len(log_loss[0])), np.array(log_loss)[0, :], 'train_step')
-            Visual.plot_loss(fig, axs, np.arange(len(log_loss[0])), np.array(log_loss)[1, :], 'valid_step')
-            fig.suptitle('training loss')
-            fig.savefig(os.path.join(train_path, 'log_loss.svg'))
-            plt.close(fig)
-
-        ################################################################
-        # Visualization
-        ################################################################
-        if epoch % 100 == 0:
-            train_source, train_true, train_pred = inference(train_loader, Net_model, Device)
-            valid_source, valid_true, valid_pred = inference(valid_loader, Net_model, Device)
-
-            torch.save(
-                {'log_loss': log_loss, 'net_model': Net_model.state_dict(), 'optimizer': Optimizer.state_dict()},
-                os.path.join(work_path, 'latest_model.pth'))
-
-            for fig_id in range(5):
-                fig, axs = plt.subplots(out_dim, 3, figsize=(18, 20), num=2)
-                Visual.plot_fields_ms(fig, axs, train_true[fig_id], train_pred[fig_id], grids)
-                fig.savefig(os.path.join(work_path, 'train_solution_' + str(fig_id) + '.jpg'))
-                plt.close(fig)
-
-            for fig_id in range(5):
-                fig, axs = plt.subplots(out_dim, 3, figsize=(18, 20), num=3)
-                Visual.plot_fields_ms(fig, axs, valid_true[fig_id], valid_pred[fig_id], grids)
-                fig.savefig(os.path.join(work_path, 'valid_solution_' + str(fig_id) + '.jpg'))
-                plt.close(fig)
+    # in_dim = 100
+    # out_dim = 8
+    # ntrain = 5000
+    # nvalid = 900
+    #
+    # batch_size = 32
+    # epochs = 1001
+    # learning_rate = 0.001
+    # scheduler_step = 800
+    # scheduler_gamma = 0.1
+    # r1 = 1
+    # print(epochs, learning_rate, scheduler_step, scheduler_gamma)
+    # #这部分应该是重采样
+    # #不进行稀疏采样
+    #
+    #
+    # ################################################################
+    # # load data
+    # ################################################################
+    # work = WorkPrj(work_path)
+    # input = design
+    # input = torch.tensor(input, dtype=torch.float)
+    #
+    # # output = fields[:, 0, :, :, :].transpose((0, 2, 3, 1))
+    # output = fields
+    # output = torch.tensor(output, dtype=torch.float)
+    #
+    # print(input.shape, output.shape)
+    #
+    # train_x = input[:ntrain]
+    # train_y = output[:ntrain]
+    # # train_g = grids[:ntrain, ::r1]
+    # valid_x = input[-nvalid:]
+    # valid_y = output[-nvalid:]
+    # # valid_g = grids[-nvalid:, ::r1]
+    #
+    # x_normalizer = DataNormer(train_x.numpy(), method='mean-std')
+    # train_x = x_normalizer.norm(train_x)
+    # valid_x = x_normalizer.norm(valid_x)
+    #
+    # y_normalizer = DataNormer(train_y.numpy(), method='mean-std')
+    # train_y = y_normalizer.norm(train_y)
+    # valid_y = y_normalizer.norm(valid_y)
+    #
+    # x_normalizer.save(os.path.join(work_path, 'x_norm.pkl'))  # 将normalizer保存下来
+    # y_normalizer.save(os.path.join(work_path, 'y_norm.pkl'))
+    #
+    # train_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(train_x, train_y),
+    #                                            batch_size=batch_size, shuffle=True, drop_last=True)
+    # valid_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(valid_x, valid_y),
+    #                                            batch_size=batch_size, shuffle=False, drop_last=True)
+    #
+    #
+    # ################################################################
+    # #  Neural Networks
+    # ################################################################
+    # with open(os.path.join('transformer_config_gvrb.yml')) as f:
+    #     config = yaml.full_load(f)
+    #     config = config['GVRB_2d']
+    #
+    # # 建立网络
+    # Tra_model = FourierTransformer(**config).to(Device)
+    # # Tra_model = SimpleTransformer(**config).to(Device)
+    # # FNO_model = FNO2d(in_dim=2, out_dim=config['n_targets'], modes=(16, 16), width=64, depth=4,
+    # #                   padding=9, activation='gelu').to(Device)
+    # MLP_model = FcnSingle(planes=(in_dim, 64, 64, config['n_targets']), last_activation=True).to(Device)
+    # Net_model = predictor(trunc=Tra_model, branch=MLP_model, field_dim=out_dim).to(Device)
+    #
+    # # model_statistics = summary(Net_model, input_size=(batch_size, train_x.shape[1]), device=Device)
+    # # Logger.write(str(model_statistics))
+    #
+    # # 损失函数
+    # Loss_func = nn.MSELoss()
+    # # Loss_func = Rotor37WeightLoss()
+    # # Loss_func = nn.SmoothL1Loss()
+    # # 优化算法
+    # Optimizer = torch.optim.Adam(Net_model.parameters(), lr=learning_rate, betas=(0.7, 0.9), weight_decay=1e-7)
+    # # 下降策略
+    # Scheduler = torch.optim.lr_scheduler.StepLR(Optimizer, step_size=scheduler_step, gamma=scheduler_gamma)
+    # # 可视化
+    # Visual = MatplotlibVision(work_path, input_name=('x', 'y'), field_name=('ps', 'ts', 'rho', 'vx', 'vy', 'vz', 'tt1', 'tt2'))
+    #
+    #
+    # star_time = time.time()
+    # log_loss = [[], []]
+    #
+    # ################################################################
+    # # train process
+    # ################################################################
+    # # grid = get_grid()
+    # # grid_real = get_grid()
+    # # grid = gen_uniform_grid(train_y[:1]).to(Device)
+    # for epoch in range(epochs):
+    #
+    #     Net_model.train()
+    #     log_loss[0].append(train(train_loader, Net_model, Device, Loss_func, Optimizer, Scheduler))
+    #
+    #     Net_model.eval()
+    #     log_loss[1].append(valid(valid_loader, Net_model, Device, Loss_func))
+    #     print('epoch: {:6d}, lr: {:.3e}, train_step_loss: {:.3e}, valid_step_loss: {:.3e}, cost: {:.2f}'.
+    #           format(epoch, learning_rate, log_loss[0][-1], log_loss[1][-1], time.time() - star_time))
+    #
+    #     star_time = time.time()
+    #
+    #     if epoch > 0 and epoch % 10 == 0:
+    #         fig, axs = plt.subplots(1, 1, figsize=(15, 8), num=1)
+    #         Visual.plot_loss(fig, axs, np.arange(len(log_loss[0])), np.array(log_loss)[0, :], 'train_step')
+    #         Visual.plot_loss(fig, axs, np.arange(len(log_loss[0])), np.array(log_loss)[1, :], 'valid_step')
+    #         fig.suptitle('training loss')
+    #         fig.savefig(os.path.join(train_path, 'log_loss.svg'))
+    #         plt.close(fig)
+    #
+    #     ################################################################
+    #     # Visualization
+    #     ################################################################
+    #     if epoch % 100 == 0:
+    #         train_source, train_true, train_pred = inference(train_loader, Net_model, Device)
+    #         valid_source, valid_true, valid_pred = inference(valid_loader, Net_model, Device)
+    #
+    #         torch.save(
+    #             {'log_loss': log_loss, 'net_model': Net_model.state_dict(), 'optimizer': Optimizer.state_dict()},
+    #             os.path.join(work_path, 'latest_model.pth'))
+    #
+    #         for fig_id in range(5):
+    #             fig, axs = plt.subplots(out_dim, 3, figsize=(18, 20), num=2)
+    #             Visual.plot_fields_ms(fig, axs, train_true[fig_id], train_pred[fig_id], grids)
+    #             fig.savefig(os.path.join(work_path, 'train_solution_' + str(fig_id) + '.jpg'))
+    #             plt.close(fig)
+    #
+    #         for fig_id in range(5):
+    #             fig, axs = plt.subplots(out_dim, 3, figsize=(18, 20), num=3)
+    #             Visual.plot_fields_ms(fig, axs, valid_true[fig_id], valid_pred[fig_id], grids)
+    #             fig.savefig(os.path.join(work_path, 'valid_solution_' + str(fig_id) + '.jpg'))
+    #             plt.close(fig)
 
 
 
---
+++ Tools/optimization/nograd_optimizer.py
@@ -0,0 +1,95 @@
+import nevergrad as ng
+import numpy as np
+import torch
+import os
+from Tools.post_process.model_predict import DLModelPost
+from Tools.post_process.load_model import rebuild_model, build_model_yml
+from Utilizes.process_data import DataNormer
+from train_model.model_whole_life import WorkPrj
+import matplotlib.pyplot as plt
+
+def fake_training(learning_rate: float, batch_size: int, architecture: str) -> float:
+    # optimal for learning_rate=0.2, batch_size=4, architecture="conv"
+    return (learning_rate - 0.2)**2 + (batch_size - 4)**2 + (0 if architecture == "conv" else 10)
+
+
+
+if __name__ == "__main__":
+    # 设置需要优化的函数
+    name = 'FNO'
+    input_dim = 28
+    output_dim = 5
+    work_load_path = os.path.join("../../Demo/Rotor37_2d", "work")
+    work_path = os.path.join(work_load_path, name)
+    work = WorkPrj(work_path)
+
+    if torch.cuda.is_available():
+        Device = torch.device('cuda')
+    else:
+        Device = torch.device('cpu')
+
+    if os.path.exists(work.x_norm):
+        norm_save_x = work.x_norm
+        norm_save_y = work.y_norm
+    else:
+        norm_save_x = os.path.join("../../Demo/Rotor37_2d", "data", "x_norm_1250.pkl")
+        norm_save_y = os.path.join("../../Demo/Rotor37_2d", "data", "y_norm_1250.pkl")
+
+    x_normlizer = DataNormer([1, 1], method="mean-std", axis=0)
+    x_normlizer.load(norm_save_x)
+    y_normlizer = DataNormer([1, 1], method="mean-std", axis=0)
+    y_normlizer.load(norm_save_y)
+
+    if os.path.exists(work.yml):
+        Net_model, inference, _, _ = build_model_yml(work.yml, Device, name=name)
+        isExist = os.path.exists(work.pth)
+        if isExist:
+            checkpoint = torch.load(work.pth, map_location=Device)
+            Net_model.load_state_dict(checkpoint['net_model'])
+    else:
+        Net_model, inference = rebuild_model(work_path, Device, name=name)
+    model_all = DLModelPost(Net_model, Device,
+                        name=name,
+                        in_norm=x_normlizer,
+                        out_norm=y_normlizer,
+                        )
+
+    targetFunc = lambda x: model_all.predictor_value(x, parameterList=["PressureLossR"])
+    # x_init = np.zeros([2, 28])
+    # Rst = targetFunc(x_init)
+    # print(Rst)
+
+    # 设置各个变量的范围
+    para = ng.p.Array(shape=(1, 28), lower=0, upper=1)
+
+    #选择并设置优化器
+    # optimizer = ng.optimizers.NGOpt10(parametrization=para, budget=200, num_workers=10)
+    optimizer = ng.optimizers.LhsDE(parametrization=para, budget=200, num_workers=10)
+
+    #获得优化结果
+    recommendation = optimizer.minimize(targetFunc)
+    print(recommendation.value)
+
+    # for i in range(10):
+    #     candidate = optimizer.ask()
+    #     value = targetFunc(candidate.value)
+    #     optimizer.tell(candidate, value)
+    #     optimizer.provide_recommendation()
+    #     print(optimizer.current_bests["minimum"])
+    #     # optimizer.watch(watch = "x", note = "iteration " + str(i))
+    loss_values = []
+    for _ in range(200):
+        # 提取当前参数
+        x = optimizer.ask()
+        # 计算损失函数值
+        loss = targetFunc(x.value)
+        # 将损失函数值添加到列表中
+        loss_values.append(loss)
+        # 提供损失函数值给优化器
+        optimizer.tell(x, loss)
+
+    # 绘制优化收敛曲线
+    plt.plot(np.arange(len(loss_values)), np.concatenate(loss_values, axis=0))
+    plt.xlabel('Iteration')
+    plt.ylabel('Loss')
+    plt.show()
